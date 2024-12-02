ca335125cffd43b28ee6b608d91f6be916b272b5
Bruno Blanes <bruno.blanes@outlook.com>
2024-12-02 19:03:59 -0300

    Moved SMTP files to the appropriate namespace

--- src/shared/Extensions/ServiceCollectionExtensions.cs
+++ src/shared/Extensions/ServiceCollectionExtensions.cs
@@ -2,29 +2,27 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
-using Netplanety.Shared.Services;
+using Netplanety.Shared.Services.Smtp;
 
 namespace Netplanety.Shared.Extensions;
 
+/// <summary>
+/// A collection of extensions to the <see cref="IServiceCollection"/> interface.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
-	public static ISmtpServiceCollection AddSmtpService(this IServiceCollection services, Action<SmtpServiceOptions> options)
+	/// <summary>
+	/// Adds and configures an SMTP service.
+	/// </summary>
+	/// <param name="services">A collection of services for the application to compose.
+	/// This is useful for adding user provided or framework provided services.</param>
+	/// <param name="options">Options to configure the SMTP client with.</param>
+	/// <returns>An <see cref="SmtpBuilder"/> for creating and configuring the SMTP system.</returns>
+	public static SmtpBuilder AddSmtpClient(this IServiceCollection services, Action<SmtpServiceOptions> options)
 	{
 		services.Configure<SmtpServiceOptions>(options);
 		services.AddSingleton<ISmtpClient, SmtpClient>();
 		services.AddSingleton<ISmtpService, SmtpService>();
-		return new SmtpServiceCollection(services);
-	}
-}
-
-public interface ISmtpServiceCollection;
-
-public sealed class SmtpServiceCollection : ISmtpServiceCollection
-{
-	public IServiceCollection Services { get; init; }
-
-	internal SmtpServiceCollection(IServiceCollection services)
-	{
-		Services = services;
+		return new SmtpBuilder(services);
 	}
 }
--- src/shared/Services/ISmtpService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Netplanety.Shared.Services;
-
-public interface ISmtpService : IAsyncDisposable
-{
-	public Task SendAsync(string name, string to, string subject, string content, CancellationToken cancellationToken);
-}
