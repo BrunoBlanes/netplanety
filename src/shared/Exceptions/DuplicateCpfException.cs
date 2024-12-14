e4cfd91615d1dc691560a7b1bdbb59483c71960a
Bruno Blanes <bruno.blanes@outlook.com>
2024-12-14 15:42:13 -0300

    Add the `DuplicateCpfException` class

---
+++ src/shared/Exceptions/DuplicateCpfException.cs
@@ -0,0 +1,11 @@
+﻿namespace Netplanety.Shared.Exceptions;
+
+public sealed class DuplicateCpfException : Exception
+{
+	public string CPF { get; init; }
+
+	public DuplicateCpfException(string cpf)
+	{
+		CPF = cpf;
+	}
+}
