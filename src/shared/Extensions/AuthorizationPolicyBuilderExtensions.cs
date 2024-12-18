df135cf79a1767361b4d186422bdd3a9ac910983
Bruno Blanes <bruno.blanes@outlook.com>
2024-12-18 20:42:33 -0300

    Add AoT support for libraries

--- src/shared/Extensions/AuthorizationPolicyBuilderExtensions.cs
+++ src/shared/Extensions/AuthorizationPolicyBuilderExtensions.cs
@@ -9,16 +9,16 @@ namespace Netplanety.Shared.Extensions;
 /// </summary>
 public static class AuthorizationPolicyBuilderExtensions
 {
-	/// <summary>
-	/// Adds a <see cref="Microsoft.AspNetCore.Authorization.Infrastructure.ClaimsAuthorizationRequirement"/> to the
-	/// current intance which requires that the current user has the <c>scope</c> claim and that the claim value must
-	/// be one of the <see cref="IdentityScopes"/> values.
-	/// </summary>
-	/// <param name="policy">The <see cref="AuthorizationPolicyBuilder"/> instance.</param>
-	/// <param name="flags">The claim value as one or more <see cref="IdentityScopes"/> flags.</param>
-	/// <returns></returns>
-	public static AuthorizationPolicyBuilder RequireScope(this AuthorizationPolicyBuilder policy, IdentityScopes flags)
-	{
-		return policy.RequireClaim("scope", flags.ToStringArray());
-	}
+    /// <summary>
+    /// Adds a <see cref="Microsoft.AspNetCore.Authorization.Infrastructure.ClaimsAuthorizationRequirement"/> to the
+    /// current intance which requires that the current user has the <c>scope</c> claim and that the claim value must
+    /// be one of the <see cref="IdentityScopes"/> values.
+    /// </summary>
+    /// <param name="policy">The <see cref="AuthorizationPolicyBuilder"/> instance.</param>
+    /// <param name="flags">The claim value as one or more <see cref="IdentityScopes"/> flags.</param>
+    /// <returns></returns>
+    public static AuthorizationPolicyBuilder RequireScope(this AuthorizationPolicyBuilder policy, IdentityScopes flags)
+    {
+        return policy.RequireClaim("scope", flags.ToStringCollection());
+    }
 }
