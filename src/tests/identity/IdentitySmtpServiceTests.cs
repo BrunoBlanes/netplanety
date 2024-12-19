e7f812132588a4856e5ed8c9521267b48fda112d
Bruno Blanes <bruno.blanes@outlook.com>
2024-12-18 21:01:30 -0300

    Add test project

---
+++ src/tests/identity/IdentitySmtpServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+using Netplanety.Identity.Services;
+using Netplanety.Identity.Tests.Mocks;
+using Netplanety.Shared.Models;
+using Netplanety.Tests.Common.Mocks;
+
+namespace Netplanety.Identity.Tests;
+
+[TestClass]
+public sealed class IdentitySmtpServiceTests
+{
+    private readonly ILogger<IdentitySmtpService> logger;
+    private readonly MockSmtpService smtpService;
+
+    public IdentitySmtpServiceTests()
+    {
+        smtpService = new MockSmtpService();
+        logger = new MockLogger<IdentitySmtpService>();
+    }
+
+    [TestMethod]
+    public async Task SendConfirmationLinkAsync_ExpectedResult()
+    {
+        var identitySmtpService = new IdentitySmtpService(smtpService, logger);
+        var user = new ApplicationUser
+        {
+
+        };
+
+        await identitySmtpService.SendConfirmationLinkAsync(user, "", "");
+    }
+}
---
+++ src/tests/identity/Mocks/MockSmtpService.cs
@@ -0,0 +1,21 @@
+﻿using Netplanety.Shared.Services.Smtp;
+
+namespace Netplanety.Identity.Tests.Mocks;
+
+internal class MockSmtpService : ISmtpService
+{
+    public MockSmtpService()
+    {
+
+    }
+
+    public Task SendAsync(string user, string address, string subject, string content, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return ValueTask.CompletedTask;
+    }
+}
